e73b74170e0fa84ec9c3fe1000168c152e261ed9
Charlie LEGER <c.leger@borea-dental.com>
2025-10-29 15:18:26 +0100

    [datas] Simplify class files
    Final barcodes can be delcared in the same pair of source files

--- src/datas/qrcode.cpp
+++ src/datas/qrcode.cpp
@@ -1,6 +1,6 @@
 #include "qbarcode/datas/qrcode.h"
 
-#include "datas/qrcode_priv.h"
+#include "datas/barcode_priv.h"
 
 /*****************************/
 /* Class documentations      */
@@ -16,6 +16,37 @@
 
 namespace qbar
 {
+
+/*****************************/
+/* Functions definitions     */
+/*      Private Class        */
+/*****************************/
+
+class QrCodePrivate : public BarcodePrivate
+{
+    Q_DECLARE_PUBLIC(QrCode)
+    QBAR_DISABLE_COPY_MOVE(QrCodePrivate)
+
+public:
+    explicit QrCodePrivate(QrCode *parent);
+    virtual ~QrCodePrivate();
+};
+
+/*****************************/
+/* Functions implementation  */
+/*      Private Class        */
+/*****************************/
+
+QrCodePrivate::QrCodePrivate(QrCode *parent)
+    : BarcodePrivate(BarType::QBAR_TYPE_QRCODE, parent)
+{
+    /* Nothing to do */
+}
+
+QrCodePrivate::~QrCodePrivate()
+{
+    /* Nothing to do */
+}
 
 /*****************************/
 /* Functions implementation  */
