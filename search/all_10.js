e1a7a74530424d60dd3e23c618075f5dfad36378
legerch <54318446+legerch@users.noreply.github.com>
2025-11-18 14:05:46 +0000

    Deploying to gh-pages from @ legerch/QBarcode@60403eba78209304f71ead69ff1760dbf1100226 🚀

--- search/all_10.js
+++ search/all_10.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['operator_28_29_0',['operator()',['../classqbar_1_1_array2_d.html#a22973a72f0a5f28d135456be93790507',1,'qbar::Array2D::operator()(size_t row, size_t col)'],['../classqbar_1_1_array2_d.html#ac6c280203f2ff7f8d75e3679d730ee68',1,'qbar::Array2D::operator()(size_t row, size_t col) const']]]
+  ['operator_28_29_0',['operator()',['../classqbar_1_1_array2_d.html#a22973a72f0a5f28d135456be93790507',1,'qbar::Array2D::operator()(size_t row, size_t col)'],['../classqbar_1_1_array2_d.html#ac6c280203f2ff7f8d75e3679d730ee68',1,'qbar::Array2D::operator()(size_t row, size_t col) const']]],
+  ['options_1',['3.2. CMake options',['../index.html#autotoc_md32-cmake-options',1,'']]]
 ];
--- search/all_8.js
+++ search/all_8.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['c_20standards_0',['2.1. C++ Standards',['../index.html#autotoc_md21-c-standards',1,'']]],
   ['clear_1',['clear',['../classqbar_1_1_payload.html#a4566fc7e1805d851e0c9a22944727fa1',1,'qbar::Payload::clear()'],['../classqbar_1_1_array2_d.html#aae83654ff93393ea3d5d3451a1c190af',1,'qbar::Array2D::clear()']]],
-  ['cmake_20usage_2',['3.1. CMake Usage',['../index.html#autotoc_md31-cmake-usage',1,'']]],
-  ['compatibility_3',['4.2.1. Compatibility',['../index.html#autotoc_md421-compatibility',1,'']]],
-  ['compilation_20time_4',['4.2.2. Compilation time',['../index.html#autotoc_md422-compilation-time',1,'']]],
-  ['create_5',['create',['../classqbar_1_1_qr_code.html#a164827bad31b8d977f356c89b66eb6d2',1,'qbar::QrCode']]]
+  ['cmake_20options_2',['3.2. CMake options',['../index.html#autotoc_md32-cmake-options',1,'']]],
+  ['cmake_20usage_3',['3.1. CMake Usage',['../index.html#autotoc_md31-cmake-usage',1,'']]],
+  ['compatibility_4',['4.2.1. Compatibility',['../index.html#autotoc_md421-compatibility',1,'']]],
+  ['compilation_20time_5',['4.2.2. Compilation time',['../index.html#autotoc_md422-compilation-time',1,'']]],
+  ['create_6',['create',['../classqbar_1_1_qr_code.html#a164827bad31b8d977f356c89b66eb6d2',1,'qbar::QrCode']]]
 ];
